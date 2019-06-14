5841500180bfea8381b9105974d4e2367708631f
Slash <slashng1992@gmail.com>
2019-06-15 02:11:31 +0300

    Updates

---
+++ precache-manifest.20af73de4e0d223aad7bc827bec9ef73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9dde3ec13178c3c07866160b057d6a",
+    "url": "/react-jun19-todo-app/index.html"
+  },
+  {
+    "revision": "cb26d4b2d0f682cc0aa6",
+    "url": "/react-jun19-todo-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-jun19-todo-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cb26d4b2d0f682cc0aa6",
+    "url": "/react-jun19-todo-app/static/js/main.e76104b6.chunk.js"
+  },
+  {
+    "revision": "5387005ef19e81b979cf",
+    "url": "/react-jun19-todo-app/static/js/runtime~main.fca6f6d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-jun19-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
